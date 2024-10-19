8e940ca3c65523f19ab3f59fbcd4bda7699b2cc3
gellenricsi <ricsi16555@gmail.com>
2024-10-19 11:55:53 +0200

    toUpperCase Service Methode finished

--- src/main/java/io/muehlbachler/bswe/StringService.java
+++ src/main/java/io/muehlbachler/bswe/StringService.java
@@ -2,12 +2,13 @@ package io.muehlbachler.bswe;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Locale;
+
 // FIXME: add javadoc
 @Service
 public class StringService {
     // FIXME: add javadoc
     public String toUpperCase(final String string) {
-        // FIXME: implement
-        return string;
+        return string.toUpperCase();
     }
 }
